2122a132377555832dce49f924ee2f82d28ce9f3
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2020-12-12 21:29:29 +0530

    Update _help.py

--- telebot/plugins/_help.py
+++ telebot/plugins/_help.py
@@ -71,7 +71,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[User Bot](https://github.com/imjanindu/TeleBot)**\n\n"""
+            help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[User Bot](https://github.com/ImJanindu/TeleBot)**\n\n"""
             try:
                 results = await bot.inline_query(  # pylint:disable=E0602
                     tgbotusername, help_string

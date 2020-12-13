9e215b3838e97a0a6fa69e7360a837594af82140
Kaveesha Induwara <72741520+kaviya-admin@users.noreply.github.com>
2020-12-13 09:37:43 +0530

    Update alive.py

--- telebot/plugins/alive.py
+++ telebot/plugins/alive.py
@@ -54,7 +54,7 @@ def get_readable_time(seconds: int) -> str:
     return ping_time
 
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "@TeleBotSupport"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "@Tech_Wizard_Ent"
 
 
 @telebot.on(admin_cmd(outgoing=True, pattern="alive"))
@@ -67,27 +67,27 @@ async def amireallyalive(alive):
     (end - start).microseconds / 1000
     uptime = get_readable_time((time.time() - StartTime))
     if ALV_PIC:
-        tele = f"**Welcome To @ImJaniya's User Bot**\n\n"
+        tele = f"**Welcome To @ImKaviya's User Bot**\n\n"
         tele += f"`{CUSTOM_ALIVE}`\n\n"
         tele += (
             f"{telemoji} **Telethon version**: `1.17`\n{telemoji} **Python**: `3.8.3`\n"
         )
         tele += f"{telemoji} **TeleBot Version**: `{telever}`\n"
-        tele += f"{telemoji} **More Info**: @InfinityJE\n"
+        tele += f"{telemoji} **More Info**: @Tech_Wizard_Ent\n"
         tele += f"{telemoji} **Sudo** : `{sudo}`\n"
         tele += f"{telemoji} **TeleBot Uptime**: `{uptime}`\n"
         tele += f"{telemoji} **Database Status**: `All OK 👌`\n"
         tele += (
             f"{telemoji} **My Pro Owner** : [{DEFAULTUSER}](tg://user?id={myid})\n\n"
         )
-        tele += "    [✨ GitHub Repository ✨](https://github.com/imjanindu/TeleBot)"
+        tele += "    [✨ GitHub Repository ✨](https://github.com/kaviya-admin/TeleBot)"
         await alive.get_chat()
         await alive.delete()
         """ For .alive command, check if the bot is running.  """
         await borg.send_file(alive.chat_id, ALV_PIC, caption=tele, link_preview=False)
         await alive.delete()
         return
-    req = requests.get("https://telegra.ph/file/311ec92a7472564551c91.jpg")
+    req = requests.get("https://telegra.ph/file/e09fb70dc8b95dd6867ea.jpg")
     req.raise_for_status()
     file = BytesIO(req.content)
     file.seek(0)
@@ -98,11 +98,11 @@ async def amireallyalive(alive):
         sticker.seek(0)
         await borg.send_message(
             alive.chat_id,
-            f"**Welcome To @ImJaniya's User Bot**\n\n"
+            f"**Welcome To @ImKaviya's User Bot**\n\n"
             f"`{CUSTOM_ALIVE}`\n\n"
             f"{telemoji} **Telethon version**: `1.17`\n{telemoji} **Python**: `3.8.3`\n"
             f"{telemoji} **TeleBot Version**: `{telever}`\n"
-            f"{telemoji} **More Info**: @TeleBotSupport\n"
+            f"{telemoji} **More Info**: @ImKaviya\n"
             f"{telemoji} **Sudo** : `{sudo}`\n"
             f"{telemoji} **TeleBot Uptime**: `{uptime}`\n"
             f"{telemoji} **Database Status**: `All OK 👌`\n"
